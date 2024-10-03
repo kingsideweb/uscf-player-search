3cc97d2342797611e4ed8e5ff449f2779a22ec13
Jacob DeJean <jacob@dejean.dev>
2024-10-03 18:43:54 -0500

    conv to cheerio

--- src/index.ts
+++ src/index.ts
@@ -1,179 +1,208 @@
-import * as puppeteer from '@cloudflare/puppeteer';
+import * as cheerio from 'cheerio';
+
+/*
+	USChessInfo represents the information to
+	be scraped from the uschess.com website
+*/
+type USChessInfo = {
+	playerName: string;
+	playerId: string;
+	playerState: string;
+	regularRating: string;
+	quickRating: string;
+	blitzRating: string;
+	expirationDt: string;
+}
+
+/*
+	PlayerStatus indicates the status of a player's
+	membership, as well as the time until expiration.
+*/
+type PlayerStatus = {
+
+	/*
+		expired is true if the expiration date scraped
+		from uschess.com is in the past.
+	*/
+	expired: boolean;
+
+	/*
+		expiresIn is the number of milliseconds until the
+		player's membership expires. This will be used to 
+		coordinate automatic renewal of memberships
+	*/
+	expiresIn: number | null;
+};
+
+/*
+	PlayerEligibility indicates whether a player is eligible
+	to participate in rated games, and if not, the reason why.
+*/
+type PlayerEligibility = {
+	eligible: boolean;
+	reason: string;
+};
+
+/*
+	PlayerData represents the data to be returned to Make
+	and stored in Memberstack. Its the result of this 
+	worker
+*/
+type PlayerData = {
+	id: string;
+	data: USChessInfo;
+	elibility: PlayerEligibility;
+};
 
 export default {
-	async fetch(request: Request, env: Env): Promise<Response> {
-		let id = env.BROWSER.idFromName('browser');
-		let obj = env.BROWSER.get(id);
-		let resp = await obj.fetch(request.url);
-		return resp;
+	async fetch(request: Request): Promise<Response> {
+		const url = new URL(request.url);
+		const id = url.searchParams.get('memberId');
+
+		if (!id) {
+			return new Response('No ID provided', { status: 400 });
+		}
+
+		try {
+			const memberInfo = await getMemberInformation(id);
+			return new Response(JSON.stringify(memberInfo), {
+				status: 200,
+				headers: {
+					'content-type': 'application/json',
+				},
+			});
+		} catch (error: any) {
+			return new Response(error.message, { status: 500, statusText: 'Internal Server Error' });
+		}
 	},
 };
 
-const KEEP_BROWSER_ALIVE_IN_SECONDS = 60;
-
-export class Browser {
-	private state: DurableObjectState;
-	private env: Env;
-	private keptAliveInSeconds: number;
-	private storage: DurableObjectStorage;
-	private browser: puppeteer.Browser | null;
-
-	constructor(state: DurableObjectState, env: Env) {
-		this.state = state;
-		this.env = env;
-		this.keptAliveInSeconds = 0;
-		this.storage = this.state.storage;
-		this.browser = null;
-	}
-
-	async fetch(request: Request): Promise<Response> {
-		if (!this.browser || !(await this.browser.isConnected())) {
-			console.log(`Browser DO: Starting new instance`);
-			try {
-				this.browser = await puppeteer.launch(this.env.MYBROWSER);
-			} catch (e) {
-				console.log(`Browser DO: Could not start browser instance. Error: ${e}`);
-				return new Response('Error starting browser', { status: 500 });
-			}
-		}
-
-		// Reset keptAlive after each call to the DO
-		this.keptAliveInSeconds = 0;
-
-		const page = await this.browser.newPage();
-
-		let response = null;
-
-		try {
-			const memberId = new URL(request.url).searchParams.get('memberId');
-			const memberIds = new URL(request.url).searchParams.get('memberIds');
-
-			if (memberId && !memberIds) {
-				response = await getMemberInformation(memberId, page);
-			}
-
-			if (memberIds && !memberId) {
-				const ids = memberIds.split(',');
-
-				if (ids.length > 5) {
-					return new Response('Too many memberIds provided. Can process up to 5 per request.', { status: 400 });
-				}
-
-				response = {
-					ids,
-					members: []
-				} as {
-					ids: Array<string>
-					members: Array<{
-						id: string,
-						data: {
-							regularRating: string | null,
-							quickRating: string | null,
-							blitzRating: string | null,
-							state: string | null,
-							expirationDt: string | null,
-						},
-						status: {
-							expired: boolean,
-							expiresIn: number | null,
-						}
-					}>
-				}
-
-				for (const id of ids) {
-					const member = await getMemberInformation(id, page);
-					response.members.push(member);
-				}
-			}
-
-			if (!memberId && !memberIds) {
-				return new Response('No memberId or memberIds query parameter provided', { status: 400 });
-			}
-
-			if (memberId && memberIds) {
-				return new Response('Both memberId and memberIds query parameters provided. Please provide only one.', { status: 400 });
-			}
-
-			await page.close();
-
-			// Set the first alarm to keep DO alive
-			let currentAlarm = await this.storage.getAlarm();
-			if (currentAlarm == null) {
-				console.log(`Browser DO: setting alarm`);
-				const TEN_SECONDS = 10 * 1000;
-				await this.storage.setAlarm(Date.now() + TEN_SECONDS);
-			}
-
-			return new Response(JSON.stringify(response, null, 2), {
-				headers: { 'Content-Type': 'application/json' },
-			});
-		} catch (error) {
-			console.error('Scraping failed:', error);
-			await page.close();
-			return new Response(`Scraping failed: ${error.message}`, { status: 500 });
-		}
-	}
-
-	async alarm(): Promise<void> {
-		this.keptAliveInSeconds += 10;
-		// Extend browser DO life
-		if (this.keptAliveInSeconds < KEEP_BROWSER_ALIVE_IN_SECONDS) {
-			console.log(`Browser DO: has been kept alive for ${this.keptAliveInSeconds} seconds. Extending lifespan.`);
-			await this.storage.setAlarm(Date.now() + 10 * 1000);
-			// You could ensure the ws connection is kept alive by requesting something
-			// or just let it close automatically when there is no work to be done
-			// for example, `await this.browser.version()`
-		} else {
-			console.log(`Browser DO: exceeded life of ${KEEP_BROWSER_ALIVE_IN_SECONDS}s.`);
-			if (this.browser) {
-				console.log(`Closing browser.`);
-				await this.browser.close();
-			}
-		}
-	}
-}
-
-async function getMemberInformation(id: string, page: puppeteer.Page) {
+async function getMemberInformation(id: string) {
 	const url = new URL('https://www.uschess.org/msa/MbrDtlMain.php');
 
 	url.search = id;
 
-	await page.goto(url.href, { waitUntil: 'networkidle0' });
+	const response = await fetch(url.toString());
 
-	async function extractRating(page: puppeteer.Page, ratingType: string, valueMatcher: (value: string) => string = (value) => value) {
-		const selector = `table[bgcolor="FFFFFF"][width="764"] table[cellspacing="9"] tr`;
-		const rows = await page.$$(selector);
-
-		for (const row of rows) {
-			const rowText = await row.evaluate((el) => el.textContent);
-			if (rowText && rowText === ratingType) {
-				const cells = await row.$$('td');
-				if (cells.length >= 2) {
-					return cells[1].evaluate((el) => el.textContent?.trim() || null).then((value) => (value ? valueMatcher(value.replace('\n', '')) : null));
-				}
-			}
-		}
-		return null;
+	if (!response.ok) {
+		throw new Error(`Failed to fetch member information for ID ${id}`);
 	}
 
-	const regularRating = await extractRating(page, 'Regular Rating', (value) => value.split(' ')[0]);
-	const quickRating = await extractRating(page, 'Quick Rating', (value) => value.split(' ')[0]);
-	const blitzRating = await extractRating(page, 'Blitz Rating', (value) => value.split(' ')[0]);
-	const state = await extractRating(page, 'State');
-	const expirationDt = await extractRating(page, 'Expiration Dt.');
+	const htmlContent = await response.text();
+
+	const chessInfo = extractChessInfo(htmlContent);
+
+	const playerStatus = {
+		expired: chessInfo.expirationDt ? new Date(chessInfo.expirationDt) < new Date() : false,
+		expiresIn: chessInfo.expirationDt ? new Date(chessInfo.expirationDt).getTime() - Date.now() : null,
+	}
+
+	// determine elibility
+	const elibility = determineEligibility(id, chessInfo, playerStatus);
 
 	return {
 		id,
-		data: {
-			regularRating,
-			quickRating,
-			blitzRating,
-			state,
-			expirationDt,
-		},
-		status: {
-			expired: expirationDt ? new Date(expirationDt) < new Date() : false,
-			expiresIn: expirationDt ? new Date(expirationDt).getTime() - Date.now() : null,
-		},
+		elibility,
+		data: chessInfo
+	};
+}
+
+function determineEligibility(requestedId: string, playerInfo: USChessInfo, playerStatus: PlayerStatus) {
+
+	// If the player wasn't found, they are not eligible
+	if (playerInfo.playerId === 'Not found') {
+		return {
+			eligible: false,
+			reason: 'player_not_found'
+		};
+	}
+
+	// If a player's membership has expired, they are not eligible
+	if (playerStatus.expired) {
+		return {
+			eligible: false,
+			reason: 'player_expired'
+		};
+	}
+
+	// If the player ID doesn't match the requested ID, they are not eligible
+	if (requestedId !== playerInfo.playerId) {
+		return {
+			eligible: false,
+			reason: 'player_invalid'
+		};
+	}
+
+	// If any of the required fields are missing, they are not eligible
+	// Non-critical fields are: playerState, quickRating, blitzRating
+	if (
+		playerInfo.playerName === 'Player not found' ||
+		playerInfo.regularRating === 'Not found' ||
+		playerInfo.expirationDt === 'Not found'
+	) {
+		return {
+			eligible: false,
+			reason: 'player_insufficient_info'
+		};
+	}
+
+	return {
+		eligible: true,
+		reason: null
+	};
+}
+
+function extractChessInfo(htmlContent: string): USChessInfo {
+	const $ = cheerio.load(htmlContent);
+
+	// Check for error message
+	const errorMessage = $('font[color="A00000"] b').text().trim();
+	const isInvalid = errorMessage.includes('Error') || errorMessage.includes('Could not retrieve data');
+
+	if (isInvalid) {
+		return {
+			playerName: 'Player not found',
+			playerId: 'Not found',
+			playerState: 'Not found',
+			regularRating: 'Not found',
+			quickRating: 'Not found',
+			blitzRating: 'Not found',
+			expirationDt: 'Not found'
+		}
+	}
+
+	// Extract id and name
+	const idAndName = $('font[size="+1"] b').text().split(': ');
+	const id = idAndName[0];
+	const fullName = idAndName[1];
+
+	// Extract ratings
+	const ratings: { [key: string]: string } = {};
+	$('tr').each((_, row) => {
+		const cells = $(row).find('td');
+		if (cells.length >= 2) {
+			const ratingType = $(cells[0]).text().trim();
+			if (['Regular Rating', 'Quick Rating', 'Blitz Rating'].includes(ratingType)) {
+				const ratingValue = $(cells[1]).find('b').text().trim().split(' ')[0];
+				ratings[ratingType] = ratingValue;
+			}
+		}
+	});
+
+	// Extract State
+	const stateFields = $('td:contains("State")').next('td').find('b').text().trim().split('\n');
+	const state = stateFields[1]
+
+	// Extract Expiration Date
+	const expirationDate = $('td:contains("Expiration Dt.")').next('td').find('b').text().trim();
+
+	return {
+		playerName: fullName || 'Player not found',
+		playerId: id || 'Not found',
+		playerState: state || 'Not found',
+		regularRating: ratings['Regular Rating'] || 'Not found',
+		quickRating: ratings['Quick Rating'] || 'Not found',
+		blitzRating: ratings['Blitz Rating'] || 'Not found',
+		expirationDt: expirationDate || 'Not found'
 	};
 }
